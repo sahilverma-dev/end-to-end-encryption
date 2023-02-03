8c1023942a010e45448cf22c27364f9170431ea7
Sahil Verma <domainsahil007@gmail.com>
2023-02-03 19:00:42 +0530

    firebase added

---
+++ src/firebase/config.ts
@@ -0,0 +1,22 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig: any = {
+  apiKey: "AIzaSyDlFSyMqJ1IZGmrOzOvO4tpRO6hc2gyGPw",
+  authDomain: "testing-edf48.firebaseapp.com",
+  databaseURL: "https://testing-edf48-default-rtdb.firebaseio.com",
+  projectId: "testing-edf48",
+  storageBucket: "testing-edf48.appspot.com",
+  messagingSenderId: "322971047410",
+  appId: "1:322971047410:web:b14b7987e49e1011ef876b",
+};
+
+const app = initializeApp(firebaseConfig);
+
+const firestore = getFirestore(app);
+const auth = getAuth(app);
+if (app) getAnalytics(app);
+
+export { auth, firestore };
