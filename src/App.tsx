09fe930b05a614c13883c1f7060f343aa05eda4b
Sahil Verma <domainsahil007@gmail.com>
2023-02-03 19:06:29 +0530

    blank routes added

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,28 @@
+import { AnimatePresence } from "framer-motion";
+import { useAuth } from "./context/AuthContext";
+import { Routes, Route, useLocation, Navigate } from "react-router-dom";
+
+// pages
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+
 const App = () => {
-  return <div>Hello</div>;
+  const { user } = useAuth();
+  const location = useLocation();
+  return (
+    <AnimatePresence initial={false}>
+      <Routes key={location.pathname}>
+        <Route
+          path="/"
+          element={user ? <Home /> : <Navigate replace to="/login" />}
+        />
+        <Route
+          path="/login"
+          element={user ? <Navigate replace to="/" /> : <Login />}
+        />
+      </Routes>
+    </AnimatePresence>
+  );
 };
 
 export default App;
